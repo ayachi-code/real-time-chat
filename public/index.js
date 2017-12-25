b25835b9c92743133b647ce6bd9eb507d827a007
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-25 15:16:34 +0100

    fix issue

--- public/index.js
+++ public/index.js
@@ -1,7 +1,5 @@
-var naam = document.getElementById('naam').value;
-
-localStorage.setItem("naam",naam)
-
 var knop_doe_mee = document.getElementById('doe_mee').addEventListener('click', () => {
+  var naam = document.getElementById('naam').value;
+  localStorage.setItem("naam",naam)
   window.open("/chat/index.html");
 });
