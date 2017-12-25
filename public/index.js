7daf8e9efe951d3cef82717d1c23926e671fa269
bilal <buddygames.bilal@gmail.com>
2017-12-25 14:51:31 +0100

    slaat naam op

--- public/index.js
+++ public/index.js
@@ -1,3 +1,7 @@
+var naam = document.getElementById('naam').value;
+
+localStorage.setItem("naam",naam)
+
 var knop_doe_mee = document.getElementById('doe_mee').addEventListener('click', () => {
   window.open("/chat/index.html");
 });
