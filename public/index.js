eb70fc5b86c6f2c8723e7c28f6dddd82b07ba8f4
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-25 15:18:40 +0100

    naam is naam_gebruiker

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 var knop_doe_mee = document.getElementById('doe_mee').addEventListener('click', () => {
-  var naam = document.getElementById('naam').value;
-  localStorage.setItem("naam",naam)
+  var naam_gebruiker = document.getElementById('naam').value;
+  localStorage.setItem("naam",naam_gebruiker)
   window.open("/chat/index.html");
 });
