cb51151a97fa841009270afc9b35b8c0da1fc50f
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-25 15:36:06 +0100

    onclick event toegevoegt aan knop

--- public/chat/index.js
+++ public/chat/index.js
@@ -1,3 +1,9 @@
 var naam_hey = localStorage.getItem("naam");
 
 document.getElementById('w').innerHTML = "Welkom " + naam_hey;
+
+
+
+var chat_knop = document.getElementById('chat_nu').addEventListener('click',() => {
+  
+});
