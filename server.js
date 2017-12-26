4584a7418f3a814987d4451b42e60ab2ff6ea072
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-26 12:01:07 +0100

    comments gemaakt om mijn code overzichtelijk te maken

--- server.js
+++ server.js
@@ -9,20 +9,26 @@ const socket = require('socket.io');
 const io = socket(server)
 
 
-
+//Lijst met gebruikers die online zijn
 var gebruikers = [];
 
 
+//Als er een connectie is
 io.sockets.on('connection',(socket) => {
 
+      //De gebruikers id van de gebruiker
       var gebruikers_id = socket.id;
 
+
+      //Als ik bericht-data ontvang gaat er een anonymous calback af.
       socket.on("bericht-data",(data) => {
           //console.log(data.naam + ": " + data.bericht);
+          //Verstuurt het naar de client
           io.emit("bericht-data",data);
 
       });
       socket.on("denaam", (data) => {
+            //pusht de naam van de gebruiker die online is
             gebruikers.push(data);
             console.log(gebruikers);
             console.log(data + " is de server gejoint");
