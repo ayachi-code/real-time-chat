eea49ae7934d4eae85fd1591b00203f883fe846d
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-27 13:05:10 +0100

    commit geplaats

--- public/chat/dechat/index.js
+++ public/chat/dechat/index.js
@@ -121,7 +121,7 @@ var online_klik_ingedrukt = document.getElementById('wie-id').addEventListener('
 
 //Als ik de-online-gebruikers-lijst
 socket.on("de-online-gebruikers-lijst",(data) => {
-  //print alle data 1 voor 1
+  //print alle data 1 voor 1 in de div
   for (var i = 0; i < data.length; i++) {
     var de_gebruikers_in_p = createP(data[i]);
     de_wie_is_online.child(de_gebruikers_in_p);
