08419e4d4f3b3b6103ed41e5f6f006e0e0f6b190
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-30 09:22:04 +0100

    event als argument gegeven

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-var knop_doe_mee = document.getElementById('doe_mee').addEventListener('click', () => {
+var knop_doe_mee = document.getElementById('doe_mee').addEventListener('click', (event) => {
   var naam_gebruiker = document.getElementById('naam').value;
   if (naam_gebruiker != "") {
     localStorage.setItem("naam",naam_gebruiker)
