b13df1d338268280b0956616cb260ad941080f41
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-30 09:33:34 +0100

    commentaar toegevoegt

--- public/index.js
+++ public/index.js
@@ -1,8 +1,12 @@
+//Als je op knop doe_mee drukt
 var knop_doe_mee = document.getElementById('doe_mee').addEventListener('click', (event) => {
   var naam_gebruiker = document.getElementById('naam').value;
+  //Als naam is niet leeg
   if (naam_gebruiker != "") {
     localStorage.setItem("naam",naam_gebruiker)
+    //Je gaat naar de chat
     window.open("/chat/index.html");
+    //Anders verschijnt er een p tag met type je naa,
   } else {
     document.getElementById('type_je_naam_gewoon').innerHTML = "Type een naam";
   }
