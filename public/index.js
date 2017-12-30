a487b4c7a9f63e1af63e6139cd03e794f67c5eaa
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-30 09:16:45 +0100

    als input fiels leeg isverschijnt er een alert

--- public/index.js
+++ public/index.js
@@ -1,5 +1,9 @@
 var knop_doe_mee = document.getElementById('doe_mee').addEventListener('click', () => {
   var naam_gebruiker = document.getElementById('naam').value;
-  localStorage.setItem("naam",naam_gebruiker)
-  window.open("/chat/index.html");
+  if (naam_gebruiker != "") {
+    localStorage.setItem("naam",naam_gebruiker)
+    window.open("/chat/index.html");
+  } else {
+    alert("Type een naam...")
+  }
 });
