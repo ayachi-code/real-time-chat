6a5cbc8a872bec17bf5da8b283cdec93eb030807
bilal <buddygames.bilal@gmail.com>
2018-01-08 18:33:11 +0100

    Als iemand de knop inklikt

--- public/register/index.js
+++ public/register/index.js
@@ -1,3 +1,8 @@
 var inloggen = document.getElementById('inloggen').addEventListener('click',() => {
   window.open("/")
 });
+
+
+var registereren = document.getElementById('klas2vh3').addEventListener('click',() => {
+  
+});
