52f70f1d048e8c951bd51c038d8b35cf55db25c4
bilal <buddygames.bilal@gmail.com>
2018-01-08 18:51:18 +0100

    value van input

--- public/register/index.js
+++ public/register/index.js
@@ -4,5 +4,8 @@ var inloggen = document.getElementById('inloggen').addEventListener('click',() =
 
 
 var registereren = document.getElementById('klas2vh3').addEventListener('click',() => {
-  
+  var email = document.getElementById('inyp').value;
+  var gebruikersnaam = document.getElementById('inyp2').value;
+  var wachtwoord = document.getElementById('inyp3').value;
+  var wachtwoord2 = document.getElementById('inyp4').value;
 });
