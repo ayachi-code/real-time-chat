b004e05a67711b9a973a09abbef2a35d2786cde0
bilal <buddygames.bilal@gmail.com>
2018-01-08 19:04:39 +0100

    mysql module geinstaleerd

--- server.js
+++ server.js
@@ -94,7 +94,8 @@ io.sockets.on('connection',(socket) => {
 
       //Als ik register-gegevens ontvang
       socket.on("register-gegevens",(data) => {
-
+          var sql = "INSERT INTO gegevens(email) VALUES('b')";
+          mysql.query(sql);
       });
 
       //Als gebruiker disconnect
