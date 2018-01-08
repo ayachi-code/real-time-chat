e35debec28675d0b913202559e9c8ba65426c0cd
bilal <buddygames.bilal@gmail.com>
2018-01-08 18:52:39 +0100

    in een object gedaan

--- public/register/index.js
+++ public/register/index.js
@@ -4,8 +4,12 @@ var inloggen = document.getElementById('inloggen').addEventListener('click',() =
 
 
 var registereren = document.getElementById('klas2vh3').addEventListener('click',() => {
-  var email = document.getElementById('inyp').value;
-  var gebruikersnaam = document.getElementById('inyp2').value;
-  var wachtwoord = document.getElementById('inyp3').value;
-  var wachtwoord2 = document.getElementById('inyp4').value;
+
+  var data = {
+    email: document.getElementById('inyp').value,
+    gebruikersnaam: document.getElementById('inyp2').value,
+    wachtwoord: document.getElementById('inyp3').value,
+    wachtwoord2: document.getElementById('inyp4').value
+  }
+
 });
