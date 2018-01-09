dd3301483b5a42ff8c9148991afd5fdd5f24cf45
bilal <buddygames.bilal@gmail.com>
2018-01-09 08:48:39 +0100

    als iemand op inloggen klikt

--- public/index.js
+++ public/index.js
@@ -1,3 +1,8 @@
 var geregistreerd = document.getElementById('register').addEventListener('click',() => {
     window.open("register/index.html");
 });
+
+
+var inloggen = document.getElementById('inloggen').addEventListener('click',() => {
+
+});
