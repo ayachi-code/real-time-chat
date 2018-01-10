fb69811b4de2f9be94bf61e1cc186bde04cb3f92
bilal <buddygames.bilal@gmail.com>
2018-01-10 13:43:37 +0100

    onodigen dingen verwijderd

--- public/chat/index.js
+++ public/chat/index.js
@@ -1,19 +1,9 @@
-var naam_hey = localStorage.getItem("naam");
-
-document.getElementById('w').innerHTML = "Welkom " + naam_hey;
-
 
 //Gaat naar de chat
 var chat_knop = document.getElementById('chat_nu').addEventListener('click',() => {
   window.open("/chat/dechat/index.html");
 });
 
-
-
-//Gaat naar online spelers
-var online_knop = document.getElementById("online").addEventListener('click',() => {
-  window.location.href = "/chat/wieisonline/"
-});
 
 //Verstuut hoe heet ik
 socket.emit("hoe-heet-ik");
